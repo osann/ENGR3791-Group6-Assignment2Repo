f77fd4f57d67dac7015774d5bd72325718205cca
osann <jh.osan@proton.me>
2023-05-05 11:53:43 +0930

    init java files

---
+++ GUI/src/StudentDatabase/DatabaseGUI.java
@@ -0,0 +1,11 @@
+package StudentDatabase;
+
+// Imports
+
+// Authors: Jack Harris,
+
+public class DatabaseGUI {
+    public static void main(String[] args) {
+        String string = "test";
+    }
+}
---
+++ StudentDatabase/src/StudentDatabase/StudentDatabase.java
@@ -0,0 +1,10 @@
+package StudentDatabase;
+
+// Imports
+
+// Authors: Jack Harris,
+public class StudentDatabase {
+    public static void main (String[] args) {
+        System.out.println("Hello, World");
+    }
+}
