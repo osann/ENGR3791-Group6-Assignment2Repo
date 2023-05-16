147d62ba8acc44db1e34d08bc86356f870b779d9
osann <44524569+osann@users.noreply.github.com>
2023-05-16 17:04:24 +0930

    Built start of Topic.java + Some test lines in StudentDatabase

--- StudentDatabase/src/StudentDatabase/StudentDatabase.java
+++ StudentDatabase/src/StudentDatabase/StudentDatabase.java
@@ -6,12 +6,21 @@ package StudentDatabase;
 
 public class StudentDatabase {
     public static void main (String[] args) {
-        // Test objects
+        // Testing Students objects
         ArtStudent testArtStudent = new ArtStudent(0000000, "Family", "given Test", "Arts", "Major Test", "Minor Test");
         MedStudent testMedStudent = new MedStudent(0000001, "Medicine", "Test SubJECT", "Medicine", "Test Prize 1988");
 
         testArtStudent.printStudent();
         System.out.println();
         testMedStudent.printStudent();
+
+        // Testing Topic objects
+        Topic testTopic = new Topic("ABCD1234", "FL");
+        System.out.println();
+        testTopic.printTopicResults();
+
+        Topic testTopic1 = new Topic("ZYXW0987", "HD", 95);
+        System.out.println();
+        testTopic1.printTopicResults();
     }
 }
--- StudentDatabase/src/StudentDatabase/Topic.java
+++ StudentDatabase/src/StudentDatabase/Topic.java
@@ -6,7 +6,75 @@ import java.lang.StringBuilder;
 // Authors: Jack Harris,
 
 public class Topic {
-    StringBuilder topicCode = new StringBuilder(8);
 
+    // Enum to handle the options for grades
+    private enum Grade {
+        FL,
+        PS,
+        CR,
+        DN,
+        HD
+    }
 
+    // Variables
+    protected StringBuilder topicCode = new StringBuilder(8);
+    protected Grade grade;
+    protected int mark;
+
+    // Default Constructor
+    public Topic(
+            String newTopicCode,
+            String newGrade) {
+
+        this.topicCode.append(newTopicCode); // TO DO: Add a check to make sure newTopicCode is 8 Char
+
+        this.grade = Grade.valueOf(newGrade); // TO DO: Add a check to make sure newGrade is 2 Char
+    }
+
+    // Constructor to handle optional marks variable
+    public Topic(
+            String newTopicCode,
+            String newGrade,
+            int newMark) {
+        // Call default constructor
+        this(newTopicCode, newGrade);
+
+        // Check if newMark > 0 and < 100
+        if (newMark < 0 || newMark > 100) {
+            throw new IllegalArgumentException();
+        } else {
+            this.mark = newMark;
+        }
+    }
+
+    // Class functions
+    public void printTopicResults(){
+        System.out.println("Topic Code: " + this.topicCode +
+                "\nGrade:      " + this.grade);
+    }
+
+    // Getters/Setters
+    public StringBuilder getTopicCode() {
+        return topicCode;
+    }
+
+    public void setTopicCode(StringBuilder topicCode) {
+        this.topicCode = topicCode;
+    }
+
+    public String getGrade() {
+        return grade.toString();
+    }
+
+    public void setGrade(Grade grade) {
+        this.grade = grade;
+    }
+
+    public int getMark() {
+        return mark;
+    }
+
+    public void setMark(int mark) {
+        this.mark = mark;
+    }
 }
