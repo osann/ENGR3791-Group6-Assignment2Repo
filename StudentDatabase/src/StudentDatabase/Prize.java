e2939e562efe330d6ac4132091808e419d0f544d
osann <44524569+osann@users.noreply.github.com>
2023-05-23 16:54:13 +0930

    Added printPrize() getters/setters

--- StudentDatabase/src/StudentDatabase/Prize.java
+++ StudentDatabase/src/StudentDatabase/Prize.java
@@ -1,17 +1,48 @@
 package StudentDatabase;
 
 public class Prize {
+    // Variables
     protected String prizeName;
-    protected StringBuilder topicCode = new StringBuilder(8);
+    protected StringBuilder topicCodeTemplate = new StringBuilder(8);
     protected int min;
 
+    // Constructor
     public Prize(
             String newPrizeName,
             String newTopicCode,
             int newMin) {
 
         this.prizeName = newPrizeName;
-        this.topicCode.append(newTopicCode);
+        this.topicCodeTemplate.append(newTopicCode);
         this.min = newMin;
+    }
+
+    // Class functions
+    public void printPrize() {
+        System.out.println("Prize: " + this.prizeName +
+                "\nTopic Template: " + this.topicCodeTemplate +
+                "\nMin: " + this.min);
+    }
+
+    // Getters/Setters
+
+    public String getPrizeName() {
+        return prizeName;
+    }
+
+    public void setPrizeName(String prizeName) {
+        this.prizeName = prizeName;
+    }
+
+    public StringBuilder getTopicCodeTemplate() {
+        return topicCodeTemplate;
+    }
+
+    public int getMin() {
+        return min;
+    }
+
+    public void setMin(int min) {
+        this.min = min;
     }
 }
