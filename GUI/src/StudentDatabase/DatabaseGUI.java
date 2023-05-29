6583c42cf590872106fd97925f9cc0d7fb193beb
mpz <zhon0110@flinders.edu.au>
2023-05-29 19:29:12 +0930

    test

--- GUI/src/StudentDatabase/DatabaseGUI.java
+++ GUI/src/StudentDatabase/DatabaseGUI.java
@@ -1,14 +1,15 @@
 package StudentDatabase;
+import javax.swing.*;
 
 // Imports
 
 // Authors: Jack Harris,
 //commit
 
-public class DatabaseGUI {
+public class DatabaseGUI extends JFrame{
     public static void main(String[] args) {
         String string = "testcommit";
         System.out.println(string);
-        //Tes
+
     }
 }
