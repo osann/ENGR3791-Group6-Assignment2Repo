e96459e5b88abc962616f358b77b18358e77ec85
mpz <zhon0110@flinders.edu.au>
2023-05-31 02:42:58 +0930

    reDo GUI

--- GUI/src/StudentDatabase/StudentDatabaseGUI.java
+++ GUI/src/StudentDatabase/StudentDatabaseGUI.java
@@ -1,64 +1,54 @@
 package StudentDatabase;
 
 import javax.swing.*;
-import java.awt.*;
 
 public class StudentDatabaseGUI extends JFrame{
 
     private JPanel mainPanel;
-    private JPanel Student ;
-    private JLabel studentDataBaseProgramLabel;
-    private JPanel DegreeOptions1;
-    private JPanel TopicDetails;
-    private JPanel AwardPries;
-    private JLabel studentNumberLabel;
+    private JLabel awardPrizesLabel;
+    private JLabel topicDetailsLabel;
+    private JLabel degreeOptionsLabel;
+    private JLabel studentDetailsLabel;
     private JTextField textField1;
     private JTextField textField2;
     private JTextField textField3;
-    private JLabel familyNameLabel;
-    private JLabel givenNameSLabel;
-    private JLabel studentDetailsLabel;
-    private JLabel degreeLabel;
-    private JComboBox DegreeComboBox;
-    private JLabel degreeOptionsLabel;
-    private JLabel artsMajiorLabel;
-    private JTextField artsMajiorTextField;
-    private JLabel artsMinorLabel;
-    private JTextField artsMinorTextField;
-    private JLabel medicinePrizesLabel;
+    private JComboBox comboBox1;
+    private JTextField textField4;
+    private JTextField textField5;
+    private JTextField textField6;
+    private JComboBox comboBox2;
+    private JTextField textField7;
+    private JTextField textField8;
+    private JTextField textField10;
+    private JTextField textField9;
+    private JTextField textField11;
     private JButton addStudentButton;
     private JButton findStudentButton;
-    private JButton enterStudentButton;
-    private JTextField MedicinePrizesTextField;
-    private JLabel topicCodeLabel;
-    private JTextField TopicCodeTextField;
-    private JLabel markLabel;
-    private JTextField MarkTextField;
-    private JLabel gradeLabel;
-    private JComboBox comboBox1;
     private JButton addTopicResultButton;
     private JButton findTopicResultButton;
-    private JLabel prizeNameLabel;
-    private JTextField PrizeNameTextField;
-    private JTextField templateTextField;
-    private JTextField NOfTtextField;
     private JButton printAllRecordsButton;
     private JButton clearAllRecordsButton;
     private JButton awardPrizeButton;
 
+
     private void createUIComponents() {
-        // TODO: place custom component creation code here
+
     }
-    public StudentDatabaseGUI(){
-        setTitle("Student DataBase Program");
-        setSize(1000,700);
+
+
+    public StudentDatabaseGUI() {
+        setTitle("Student Database Program");
+        setSize(1500, 500);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        createUIComponents();
+
+        setContentPane(mainPanel);
         setVisible(true);
-
-
     }
+
     public static void main(String[] args) {
-        StudentDatabaseGUI myFrame = new StudentDatabaseGUI();
+        StudentDatabaseGUI myFr = new StudentDatabaseGUI();
     }
 
 }
